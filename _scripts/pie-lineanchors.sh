3a760145fa5da8696af0e899f9c73037da55fc34
Drew Silcock <as1423@my.bristol.ac.uk>
2014-08-05 17:40:52 +0100

    Turn post names into variable

--- _scripts/pie-lineanchors.sh
+++ _scripts/pie-lineanchors.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 languages=( vim python perl ruby yaml css html bash cpp c )
+posts="../_posts/*"
 
 for lang in "${languages[@]}"
 do
     :
-    perl -pi -e "s/highlight $lang/highlight $lang lineanchors/g" _posts/*
+    perl -pi -e "s/highlight $lang/highlight $lang lineanchors/g" $posts
 done
