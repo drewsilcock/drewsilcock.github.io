363ae5222ef375f39197e6e6562addaaa877fc9e
drewsberry <drew@drewsilcock.co.uk>
2014-10-15 14:03:08 +0100

    Remove old katexify plugin and finish katex_block

--- _plugins/katex_block.rb
+++ _plugins/katex_block.rb
@@ -1,5 +1,4 @@
 require 'execjs'
-require 'cgi'
 
 module Jekyll
     module Tags
@@ -12,17 +11,34 @@ module Jekyll
                 @markup = markup
 
                 @centred = markup.include? 'centred'
-
-                path_to_katex = "./public/js/katex.min.js"
-                katexsrc = open(path_to_katex).read
-                @katex = ExecJS.compile(katexsrc)
             end
 
+            # Config defaults
+            PATH_TO_JS = "./public/js/katex.min.js"
+
             def render(context)
+                # Take config from global `_config.yml', if it exists
+                katex_config = context.registers[:site].config['katex'] || {}
+
+                @config = {}
+
+                # Take path to KaTeX javascript from site config or default
+                @config['path_to_js'] = katex_config['path_to_js'] || PATH_TO_JS
+
+                import_js(@config['path_to_js'])
+
+                latex_source = super
+
                 if @centred
-                    return katexify_centred(super)
+                    return katexify_centred(latex_source)
                 end
-                katexify_inline(super)
+
+                katexify_inline(latex_source)
+            end
+
+            def import_js(path)
+                katexsrc = open(path).read
+                @katex = ExecJS.compile(katexsrc)
             end
 
             def katexify_inline(content)
@@ -30,7 +46,6 @@ module Jekyll
             end
 
             def katexify_centred(content)
-                style = "text-align: center;"
                 div_open = "<span class='centredequation'>"
                 div_close = "</span>"
 
